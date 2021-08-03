3b78927fd6c535fc2ae179d6b587b78cb6f23268
Ankeet Guha <ankeetguha@Ankeets-MacBook-Pro.local>
2021-08-03 10:27:39 +0530

    Adding prerender.io middleware for dynamic OG tags

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const app = express()
 //here we are configuring dist to serve app files
 app.use('/', serveStatic(path.join(__dirname, '/dist')))
 
+app.use(require('prerender-node').set('prerenderToken', '3HqwTfWFhRA9r1suau8U'));
+
 // this * route is to serve project on different page routes except root `/`
 app.get(/.*/, function (req, res) {
 	res.sendFile(path.join(__dirname, '/dist/index.html'))
--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,7 @@ module.exports = {
         config
             .plugin('html')
             .tap(args => {
-                args[0].title = "Skipper";
+                args[0].title = "Skipper - Launch Your Insta Website";
                 return args;
             })
     },
