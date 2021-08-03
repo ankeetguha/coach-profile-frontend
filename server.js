a59ac228ebaaf8a5b4944bb6d6c9eb011f6ba68e
Ankeet Guha <ankeetguha@Ankeets-MacBook-Pro.local>
2021-08-03 10:31:44 +0530

    Adding prerender.io middleware for dynamic OG tags

--- server.js
+++ server.js
@@ -5,9 +5,8 @@ const path = require('path')
 const app = express()
 
 //here we are configuring dist to serve app files
-app.use('/', serveStatic(path.join(__dirname, '/dist')))
-
 app.use(require('prerender-node').set('prerenderToken', '3HqwTfWFhRA9r1suau8U'));
+app.use('/', serveStatic(path.join(__dirname, '/dist')))
 
 // this * route is to serve project on different page routes except root `/`
 app.get(/.*/, function (req, res) {
